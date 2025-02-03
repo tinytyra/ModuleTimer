c247c954fc046644db209cf0d187416459dfa779
tinytyra <39837549+tinytyra@users.noreply.github.com>
2025-02-03 14:04:27 +0100

    it workle

--- godot/scripts/timer.gd
+++ godot/scripts/timer.gd
@@ -1,18 +1,15 @@
 extends Timer
 
 func _ready() -> void:
-	start(5)
-	
+	get_parent().user_duration_is.connect(_on_user_duration_is)
+
+func _on_user_duration_is(duration):
+	start(duration)
+
 func _physics_process(_delta: float) -> void:
 	$Label.set_text(str(round(time_left)))
 	if time_left == 0:
 		$Label.set_text("DING!")
-	
-	# debug
-	print(round(time_left))
 
 func _on_timeout() -> void:
 	stop()
-	
-	# debug
-	print("DING!")
