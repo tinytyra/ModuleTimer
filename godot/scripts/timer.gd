af8590a7f6ca5ddf6560646b2b2a0ffda465349b
tinytyra <39837549+tinytyra@users.noreply.github.com>
2025-02-03 11:30:26 +0100

    timer node

---
+++ godot/scripts/timer.gd
@@ -0,0 +1,18 @@
+extends Timer
+
+func _ready() -> void:
+	start(3)
+	
+func _physics_process(_delta: float) -> void:
+	$Label.set_text(str(round(time_left)))
+	if time_left == 0:
+		$Label.set_text("DING!")
+	
+	# debug
+	print(round(time_left))
+
+func _on_timeout() -> void:
+	stop()
+	
+	# debug
+	print("DING!")
