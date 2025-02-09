c8564dc61afa7a999d60a3b2fcef7e2dd918b599
tinytyra <39837549+tinytyra@users.noreply.github.com>
2025-02-09 11:28:44 +0100

    Revert "im too sleep deprived for this"
    This reverts commit 784f13930c74eb15b605538977c38084fc16e744.

--- godot/scripts/timer.gd
+++ godot/scripts/timer.gd
@@ -1,32 +1,15 @@
 extends Timer
 
-var duration
-var timestamp : String
-
 func _ready() -> void:
 	get_parent().user_duration_is.connect(_on_user_duration_is)
-	print("timer has:")
-	print(duration)
 
 func _on_user_duration_is(duration):
 	start(duration)
 
 func _physics_process(_delta: float) -> void:
-	convert_duration()
-	update_label()
-#	$Label.set_text(str(round(time_left)))
+	$Label.set_text(str(round(time_left)))
+	if time_left == 0:
+		$Label.set_text("DING!")
 
-
-# convert duration in seconds to HH:MM:SS
-func convert_duration():
-	#var seconds = (duration%60)
-	#var minutes = (duration/60)%60
-	#var hours = (duration/60)/60
-	
-	#timestamp = "%02d:%02d:%02d" % [hours, minutes, seconds]
-	timestamp = "69:42:00"
-	
-	#print("%02d:%02d:%02d" % [hours, minutes, seconds])
-
-func update_label():
-	$Label.set_text(timestamp)
+func _on_timeout() -> void:
+	stop()
